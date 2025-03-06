9c6c24aadee55eac48be37e626e2406b9d68a313
COMPTES\mohamem2 <Moez.MOHAMED.ext@ag2rlamondiale.fr>
2025-03-06 23:50:24 +0100

    fixer un souci lié au model et architecture

--- src/app/shared/model/product.ts
+++ src/app/shared/model/product.ts
@@ -4,10 +4,6 @@ export interface Product {
     id: number;
     productName: string;
     price: number;
-    quantity: number;
     isImported: boolean;
     category: Category;
-    taxes: number;
-    priceTTC: number;
-    nbrArticleAddedToBasket: number;
 }
